9f950938ecd1a52a9844a223380db5a34d4d711e
Bruno <bsgiovanini@gmail.com>
2014-10-17 10:34:22 -0300

    Implementacao do SVM

--- kdd/Main.py
+++ kdd/Main.py
@@ -1,4 +1,6 @@
 from sklearn.decomposition import PCA
+from sklearn import svm
+import multiprocessing
 import pandas as pd
 
 
@@ -17,7 +19,18 @@ class Main:
         self.carregaContextosDF()
 
         for base in self.bases:
+
+            print "base: ", base.nome
+
             for numComponentesPCA in range(1, self.numMaxComponentesPCA + 1):
+
+                print "numero de componentes PCA: ", numComponentesPCA
+
+                processos = []
+                resultados = []
+
+                classifier = svm.SVC(gamma=0.01, C=100.)
+
                 for fold in range(1, self.numFolds + 1):
                     conjuntos = self.separaConjuntosDF(fold, base.dados)
 
@@ -27,64 +40,37 @@ class Main:
                     X = conjuntos['teste'].as_matrix(conjuntos['teste'].columns[0:-2])
                     testePCA = modeloPCA.transform(X)
 
-                    self.classificaSVM(treinoPCA, conjuntos['treino'])
-                    self.classificaSVM(testePCA, conjuntos['teste'])
+                    classesTreino = conjuntos['treino']['classe']
+                    classesTeste = conjuntos['teste']['classe']
+
+                    # appenda os resultados obtidos para cada fold para futura media
+                    resultados.append(self.classificaSVM(classifier, processos, treinoPCA, classesTreino, testePCA, classesTeste))
+
+                for p in processos:
+                    p.join()
+
+                accuracy = sum(resultados)/len(resultados)
+
+                print "acuracia para o numero de componentes", accuracy
 
 
-    # def calculaTransformacaoPCA(self, dados, numComponentes):
-    #
-    # dadosPCA = []
-    #
-    # for tupla in dados:
-    # tuplaPCA = list([x for x in tupla[0:-2]])
-    # dadosPCA.append(tuplaPCA)
-    # pca = PCA(n_components=numComponentes)
-    # X = np.array(dadosPCA)
-    #     return pca.fit_transform(X)
 
-    def classificaSVM(self, dadosPCA, dados):
+    def get_classifier_accuracy(self, classifier, treinoPCA, classesTreino, testePCA, classesTeste, out_score):
+        #realiza a classificacao e determina o percentual em cima dos dados de teste
+        score = classifier.fit(treinoPCA, classesTreino).score(testePCA, classesTeste)
 
-        # i = 0
-        #
-        # dadosSVM = []
-        # for tupla in dadosPCA:
-        #     tuplaSVM = np.append(tupla, [dados[i][-2]])
-        #     dadosSVM.append(tuplaSVM)
-        #     i += 1
+        #pega os resultados
+        out_score.put(score)
+        return
 
-        print "falta classificar!!! "
+    def classificaSVM(self, classifier, processos, treinoPCA, classesTreino,  testePCA, classesTeste):
+        out_q = multiprocessing.Queue()
+        p = multiprocessing.Process(target=Main.get_classifier_accuracy, args=(self, classifier, treinoPCA, classesTreino,  testePCA, classesTeste,  out_q))
+        processos.append(p)
+        p.start()
+        return out_q.get()
 
 
-    # def carregaContextos(self):
-    #     dadosDblp = []
-    #     dadosAmazon = []
-    #     dadosFlickr = []
-    #
-    #     with open('../dados/dblp.csv') as f:
-    #         for line in csv.reader(f, delimiter=";"):
-    #             item = list([float(x) for x in line[:]])
-    #             dadosDblp.append(item)
-    #
-    #     with open('../dados/amazon.csv') as f:
-    #         for line in csv.reader(f, delimiter=";"):
-    #             item = list([float(x) for x in line[:]])
-    #             dadosAmazon.append(item)
-    #
-    #     with open('../dados/flickr.csv') as f:
-    #         for line in csv.reader(f, delimiter=";"):
-    #             item = list([float(x) for x in line[:]])
-    #             dadosFlickr.append(item)
-    #
-    #     self.bases.append(BaseDados("dblp", dadosDblp))
-    #     self.bases.append(BaseDados("amazon", dadosAmazon))
-    #     self.bases.append(BaseDados("flickr", dadosFlickr))
-
-    # def separaConjuntos(self, fold, dados):
-    #     conjuntos = {}
-    #     fold = float(fold)
-    #     conjuntos['treino'] = [elem for elem in dados if elem[-1] != fold]
-    #     conjuntos['teste'] = [elem for elem in dados if elem[-1] == fold]
-    #     return conjuntos
 
     def carregaContextosDF(self):
         dadosDblpDF = pd.read_csv('../dados/dblp.csv', sep=';', header=None, names=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', 'classe', 'fold'])
@@ -92,8 +78,8 @@ class Main:
         dadosFlickrDF = pd.read_csv('../dados/flickr.csv', sep=';', header=None, names=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', 'classe', 'fold'])
 
         self.bases.append(BaseDados("dblp", dadosDblpDF))
-        self.bases.append(BaseDados("amazon", dadosAmazonDF))
-        self.bases.append(BaseDados("flickr", dadosFlickrDF))
+        #self.bases.append(BaseDados("amazon", dadosAmazonDF))
+        #self.bases.append(BaseDados("flickr", dadosFlickrDF))
 
 
     def separaConjuntosDF(self, fold_num, dados):
@@ -106,7 +92,6 @@ class Main:
         pca = PCA(n_components=numComponentes)
         X = dados.as_matrix(dados.columns[0:-2])
         #A linha a seguir ja transforma o X em PCA, nos precisamos do modelo
-        # return pca.fit_transform(X)
         #Agora nos temos o modelo sendo retornado assim como o X transformado
         pca.fit(X)
         return pca, pca.transform(X)
